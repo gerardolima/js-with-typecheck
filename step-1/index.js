539a12bb541083c9c478dc4e25481858da4fa82a
Gerardo Lima <gerardo.lima@gmail.com>
2020-11-17 13:18:06 +0000

    Add `mover`, for complex object checking

--- step-1/index.js
+++ step-1/index.js
@@ -3,10 +3,22 @@ const doubler = x => {
   return x *2;
 }
 
-// this function causes a run-time error
-const offender = () => doubler('aa');
+const mover = (point, delta) => {
+  if(typeof(point.x) !== "number") throw new TypeError();
+  if(typeof(point.y) !== "number") throw new TypeError();
+  if(typeof(delta.x) !== "number" && typeof(delta.x) !== "undefined") throw new TypeError();
+  if(typeof(delta.y) !== "number" && typeof(delta.y) !== "undefined") throw new TypeError();
+
+  return { x: point.x + (delta.x || 0), y: point.y + (delta.y || 0) }
+}
+
+// these functions cause run-time errors
+const doublerOffender = () => doubler('aa');
+const moverOffender = () => mover({x: 1, y: 2}, {x:'a'});
 
 module.exports = {
   doubler,
-  offender
+  mover,
+  doublerOffender,
+  moverOffender,
 };
--- step-1/index.spec.js
+++ step-1/index.spec.js
@@ -1,5 +1,5 @@
 const {expect} = require('chai');
-const {doubler} = require('./index');
+const {doubler, mover} = require('./index');
 
 describe('step-1', () => {
 
@@ -14,4 +14,32 @@ describe('step-1', () => {
     });
 
   });
+
+  describe('mover', () => {
+
+    it('does not move on empty delta', () => {
+      expect(mover({x: 1, y: 2}, {})).to.deep.equal({x: 1, y: 2});
+    });
+
+    it('move on x axis', () => {
+      expect(mover({x: 1, y: 2}, {x: 10})).to.deep.equal({x: 11, y: 2});
+    });
+
+    it('move on y axis', () => {
+      expect(mover({x: 1, y: 2}, {y: 100})).to.deep.equal({x: 1, y: 102});
+    });
+
+    it('move on both axis', () => {
+      expect(mover({x: 1, y: 2}, {x: 10, y: 100})).to.deep.equal({x: 11, y: 102});
+    });
+
+    it('throws exceptions on type mismatches', () => {
+      expect(()=>mover({x: 'a', y: 2}, {x: 10, y: 100})).to.throw(TypeError);
+      expect(()=>mover({x: 1, y: 'a'}, {x: 10, y: 100})).to.throw(TypeError);
+      expect(()=>mover({x: 1, y: 2}, {x: 'a', y: 100})).to.throw(TypeError);
+      expect(()=>mover({x: 1, y: 2}, {x: 10, y: 'a'})).to.throw(TypeError);
+    });
+
+  });
+
 });
